ef25b64de7ea9f8b4a3190b5c0bb9b20a52f361d
Alfie Richards <alfier@wolfram.com>
2022-12-01 14:53:02 +0000

    Fixed semantic def

--- lean/incorrectness_logic.lean
+++ lean/incorrectness_logic.lean
@@ -20,57 +20,72 @@ inductive LogicType : Type
 | er
 | ok
 
-inductive post: IncLoLang.stmt -> LogicType -> (IncLoLang.state) -> (IncLoLang.state) -> Prop
-| seq_er {S T s t u} (H1: post S LogicType.ok s t) (H2: post T LogicType.er t u) : /- TODO: This needs to use post norm instead? How to encode? -/
-  post (S ;; T) LogicType.er s u
-| error {s}:
-  post IncLoLang.stmt.error LogicType.er s s
+inductive lang_semantics: IncLoLang.stmt -> LogicType -> (IncLoLang.state) -> (IncLoLang.state) -> Prop
 | skip {s} :
-  post IncLoLang.stmt.skip LogicType.ok s s
+  lang_semantics IncLoLang.stmt.skip LogicType.ok s s
+| seq_ok {S T s t u} (H1: lang_semantics S LogicType.ok s t) (H2: lang_semantics T LogicType.ok t u) :
+  lang_semantics(S ;; T) LogicType.ok s u
+| seq_er {S T s t u} (H1: lang_semantics S LogicType.ok s t) (H2: lang_semantics T LogicType.er t u) : 
+  lang_semantics (S ;; T) LogicType.er s u
+| error {s}:
+  lang_semantics IncLoLang.stmt.error LogicType.er s s
 | assign {x s f} :
-  post (IncLoLang.stmt.assign x f) LogicType.ok s (s{x ↦ (f s)})
-| seq_ok {S T s t u} (H1: post S LogicType.ok s t) (H2: post T LogicType.ok t u) :
-  post(S ;; T) LogicType.ok s u
+  lang_semantics (IncLoLang.stmt.assign x f) LogicType.ok s (s{x ↦ (f s)})
 
-def incorrectness_logic (ty: LogicType) (P : (IncLoLang.state) → Prop) (R : IncLoLang.stmt)
-  (Q : (IncLoLang.state) → Prop) : Prop := 
-  ∀ s t, Q s -> post R ty t s -> P t
+def post (ty: LogicType) (r: IncLoLang.stmt) (P: IncLoLang.state -> Prop) : IncLoLang.state -> Prop 
+  := λ st, ∃ σ, P σ ∧ lang_semantics r ty σ st
 
-def hoare_logic (ty: LogicType) (P : (IncLoLang.state) → Prop) (R : IncLoLang.stmt)
+/- TODO: Change encoding of state-/
+/- state × ℕ -> prop -/
+
+def incorrectness_logic (type: LogicType) (P : (IncLoLang.state) → Prop) (R : IncLoLang.stmt)
   (Q : (IncLoLang.state) → Prop) : Prop := 
-  ∀ s t, P s -> post R ty t s -> Q t
+  ∀ state, Q state -> ((post type R) P) state
+
+def hoare_logic (type: LogicType) (P : (IncLoLang.state) → Prop) (R : IncLoLang.stmt)
+  (Q : (IncLoLang.state) → Prop) : Prop := 
+  ∀ state, ((post type R) P) state -> Q state
 
 notation `{* ` P : 1 ` *} ` S : 1 ` {* ` Q : 1 ` *}` ty: 2 :=
   hoare_logic ty P S Q
 
 notation `[* ` P : 1 ` *] ` S : 1 ` [* ` Q : 1 ` *]` ty: 2 :=
-  hoare_logic ty P S Q
+  incorrectness_logic ty P S Q
 
 lemma hoare_skip_intro_ok { P: IncLoLang.state -> Prop } : {* P *} IncLoLang.stmt.skip {* P *} LogicType.ok:=
 begin
-  intros s t hs hst,
-  cases hst,
-  assumption,
+  intros state h,
+
+  rcases h with ⟨σ, ⟨h₁, h₂⟩⟩,
+
+  cases h₂,
+  finish,
 end
 
-lemma hoare_skip_intro_er { P: IncLoLang.state -> Prop } : {* P *} IncLoLang.stmt.skip {* P *} LogicType.er :=
+lemma inc_skip_intro_er { P: IncLoLang.state -> Prop } : [* P *] IncLoLang.stmt.error [* P *] LogicType.er :=
 begin
-  intros s t hs hst,
-  cases hst,
+  intros state h,
+
+  use state,
+
+  split,
+  {finish,},
+  {
+    exact lang_semantics.error
+  },
 end
 
-
-lemma inc_skip_intro_ok { P: IncLoLang.state -> Prop } : [* P *] IncLoLang.stmt.skip [* P *] LogicType.ok:=
-begin
-  intros s t hs hst,
-  cases hst,
-  assumption,
-end
-
-lemma inc_skip_intro_er { P: IncLoLang.state -> Prop } : [* P *] IncLoLang.stmt.skip [* P *] LogicType.er :=
-begin
-  intros s t hs hst,
-  cases hst,
-end
+-- lemma seq_intro_hoare {P Q R S T} (hS : {* P *} S {* Q *} LogicType.ok)
+--     (hT : {* Q *} T {* R *} LogicType.ok) :
+--   {* P *} S ;; T {* R *} LogicType.ok :=
+-- begin
+--   intros s t hs hst,
+--   cases' hst,
+--   apply hT,
+--   { apply hS,
+--     { exact hs },
+--     { assumption } },
+--   { assumption }
+-- end
 
 end IncLoIncorrectness
--- lean/language.lean
+++ lean/language.lean
@@ -17,34 +17,18 @@ namespace IncLoLang
 
 meta def tactic.dec_trivial := `[exact dec_trivial]
 
-def state : Type := string -> ℕ
+def state : Type := string × ℕ -> Prop
 
 def state.update : string -> ℕ -> state -> state
-| name val s          := (λname', if name' = name then val else s name')
+| name val s := (λ pair, pair == (name, val) ∨ s pair)
 
 notation s `{` name ` ↦ ` val `}` := state.update name val s
 
 @[simp] lemma update_apply (name : string) (val : ℕ) (s : state) :
-  s{name ↦ val} name = val :=
+  s{name ↦ val} (name, val) :=
 begin
-  calc s{name ↦ val} name = (λname', if name' = name then val else s name') name : by refl
-       ...                = val : by finish,
-end
-
-@[simp] lemma update_apply_ne (name name' : string) (val : ℕ) (s : state) 
-  (h : name' ≠ name . tactic.dec_trivial) : 
-    s{name ↦ val} name' = s name' :=
-begin
-  calc s{name ↦ val} name' = (λname', if name' = name then val else s name') name' : by refl
-       ...                 = s name' : by finish[h],
-end
-
-@[simp] lemma update_override (name : string) (val₁ val₂ : ℕ) (s : state) :
-  s{name ↦ val₂}{name ↦ val₁} = s{name ↦ val₁} :=
-begin
-  apply funext,
-  intro name',
-  by_cases name' = name; simp [h]
+  unfold state.update,
+  finish,
 end
 
 /-! ## Language -/
@@ -57,25 +41,6 @@ inductive stmt : Type
 -- | ite    : (state → Prop) → stmt → stmt → stmt
 -- | while  : (state → Prop) → stmt → stmt
 
-def eval : option state -> stmt -> option state
-| st stmt.skip                  := st
-| none (stmt.assign v s)        := none
-| (some st) (stmt.assign v s)   := st{v ↦ s st}
-| st (stmt.seq n m)             := (eval (eval st n) m)
-| st (stmt.error)             := none
-
 infixr ` ;; ` : 90 := stmt.seq
-
-#eval option.bind (eval (some (λ x, 0)) stmt.skip) (λ s, some (s "x"))
-#eval option.bind (eval (some (λ x, if x = "y" then 2 else 0)) (stmt.assign "x" (λ s, 1 + s "y"))) 
-  (λ s, some (s "x"))
-#eval option.bind (eval (some (λ x, if x = "y" then 2 else 0))
-  ((stmt.assign "x" (λ s, 1 + s "y")) ;; 
-   (stmt.assign "x" (λ s, 1 + s "x")))) (λ s, some (s "x"))
-
-#eval option.bind (eval (none)
-  ((stmt.assign "x" (λ s, 1 + s "y")) ;; 
-   (stmt.assign "x" (λ s, 1 + s "x")))) (λ s, some (s "x"))
-#eval option.bind (eval (some (λ x, 0)) stmt.error) (λ s, some (s "x"))
 
 end IncLoLang
