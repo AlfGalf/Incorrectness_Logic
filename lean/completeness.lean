0f72d172c13f10fff4c39c501c3f7058b8555cd9
Alfie Richards <alfier@wolfram.com>
2023-03-16 13:33:32 +0100

    Cleanup

--- lean/completeness.lean
+++ lean/completeness.lean
@@ -2,21 +2,6 @@ import lean.incorrectness_logic
 
 namespace IncorrectnessCompleteness
 
--- inductive IncorrectnessProof (P Q: IncLoLang.prop) (C: IncLoLang.stmt) (ty: IncLoLang.LogicType): Prop
--- | empty_under_approx : (ty = IncLoLang.LogicType.er) → IncorrectnessProof
--- | consequence        : (∃ P' Q': IncLoLang.prop, (∀ σ, P' σ  → P σ) ∧ (∀ σ, Q σ  → Q' σ) ∧ (IncorrectnessProof P' Q' C ty)) → Prop
--- | disjunction        : (∃ P₁ P₂ Q₁ Q₂, (P = P₁ ∧ P₂) ∧ (Q = Q₁ ∧ Q₂) ∧ ([* P₁ *]C[* Q₁ *]ty) ∧ ([* P₁ *]C[* Q₁ *]ty)) → Prop
--- | unit_ok            : (C = IncLoLang.stmt.skip) → (ty = IncLoLang.LogicType.ok) → (P = Q) → IncorrectnessProof
--- | unit_er            : (C = IncLoLang.stmt.skip) → (ty = IncLoLang.LogicType.er) → (Q = λ σ, false) → IncorrectnessProof
--- | sequencing_short   : (∃ C₁ C₂, C = C₁ ;; C₂ ∧ ty=IncLoLang.LogicType.er ∧ [*P*]C₁[*Q*]IncLoLang.LogicType.er) → IncorrectnessProof
--- | sequencing_normal  : (∃ C₁ C₂ R, C = C₁ ;; C₂ ∧ ([*P*]C₁[*R*]IncLoLang.LogicType.ok) ∧ [*R*]C₂[*Q*]ty) → IncorrectnessProof
--- | iterate_zero       : (∃ D, C = D**) → (ty = IncLoLang.LogicType.ok) → (P = Q) → IncorrectnessProof
--- | backwards_vairent  : (ty = IncLoLang.LogicType.ok) → (∃ (N: ℕ) (R: ℕ → IncLoLang.prop), P = R 0 ∧ Q = R N ∧ (∀ n: ℕ, [* R n *]C[* R (n + 1) *]ty)) → IncorrectnessProof
--- | choice             : (∃ C₁ C₂, C = C₁ <+> C₂ ∧ (([*P*]C₁[*Q*]ty) ∨ ([*P*]C₂[*Q*]ty))) → IncorrectnessProof
--- | error_er           : (C = IncLoLang.stmt.error ∧ ty = IncLoLang.LogicType.er ∧ P = Q) → IncorrectnessProof
--- | error_ok           : (C = IncLoLang.stmt.error ∧ ty = IncLoLang.LogicType.ok ∧ Q = λ σ, false) → IncorrectnessProof
--- | assumes            : (C = IncLoLang.stmt.error ∧ ty = IncLoLang.LogicType.ok ∧ Q = λ σ, false) → IncorrectnessProof
-
 inductive IncorrectnessProof : IncLoLang.prop → IncLoLang.stmt → IncLoLang.prop → IncLoLang.LogicType → Prop
 | empty_under_approx {P: IncLoLang.prop} {ty: IncLoLang.LogicType} {C: IncLoLang.stmt}: 
   IncorrectnessProof P C (λ _, false) ty
@@ -87,7 +72,7 @@ begin
   induction h,
 
   case empty_under_approx { exact IncLogic.empty_under_incorrect, },
-  case consequence { exact IncLogic.consequence_incorrect (h_ih) h_Hq h_Hp, },
+  case consequence { exact IncLogic.consequence_incorrect h_ih h_Hq h_Hp, },
   case disjunction { refine IncLogic.disjunction_incorrect h_ih_H₁ h_ih_H₁_1},
   case unit_ok { refine IncLogic.unit_incorrect_ok},
   case unit_er { refine IncLogic.unit_incorrect_err},
